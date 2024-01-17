176a4ec8b46ba49a5d18cf89ff6414c4caebf98a
Oluwagbemiga David <39445905+oluwagbemigadavid@users.noreply.github.com>
2024-01-17 07:33:26 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import { Search, ShoppingBasket, UserRound } from 'lucide-react';
+import Mini from './components/Mini/Mini';
 function App() {
   return (
     <div>
       <div className="hero">
         <div className="hero__container">
+
           <div className="hero__container__lines">
             <div></div>
             <div></div>
@@ -13,6 +15,7 @@ function App() {
             <div></div>
             <div></div>
           </div>
+
           <section className="hero__container__top">
             <div className="hero__container__top__logo">zainoo&trade;</div>
             <div className="hero__container__top__icons">
@@ -20,6 +23,12 @@ function App() {
                <ShoppingBasket />
                <UserRound />
             </div>
+          </section>
+
+          <section className="hero__container__contents">
+            <div className="hero__container__contents__left"><Mini /></div>
+            <div className="hero__container__contents__mid">baba</div>
+            <div className="hero__container__contents__right">saba</div>
           </section>
         </div> 
       </div>
---
+++ src/components/Mini/Mini.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Mini = ({mini, currentView}) => {
+  return (
+    <div className="mini">
+      <div className="mini__container">
+
+      </div>
+    </div>
+  )
+}
+
+export default Mini
