a884c467a3a155b99e6a047f6ade0f81351cac64
Oluwagbemiga David <39445905+oluwagbemigadavid@users.noreply.github.com>
2024-01-17 07:15:13 +0100

    Top done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 
-
+import { Search, ShoppingBasket, UserRound } from 'lucide-react';
 function App() {
   return (
     <div>
       <div className="hero">
         <div className="hero__container">
-          Testing font face
+          <div className="hero__container__lines">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <section className="hero__container__top">
+            <div className="hero__container__top__logo">zainoo&trade;</div>
+            <div className="hero__container__top__icons">
+               <Search />
+               <ShoppingBasket />
+               <UserRound />
+            </div>
+          </section>
         </div> 
       </div>
     </div>
