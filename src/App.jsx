9e92fcc94d87ac1da7c118eeb903864d2cc52809
Oluwagbemiga David <39445905+oluwagbemigadavid@users.noreply.github.com>
2024-01-17 06:49:33 +0100

    Site created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div>
+      <div className="hero">
+        <div className="hero__container">
+          
+        </div> 
+      </div>
+    </div>
+  )
+}
+
+export default App
